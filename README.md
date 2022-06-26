9c9f056bd6e57690d9a40b7cad583d9d5883854a
Hai Trieu Ngoc <trieungochai.dev@gmail.com>
2022-06-26 15:10:19 +0700

    Setting up our Security Example

---
+++ README.md
@@ -0,0 +1,16 @@
+## Digital Certificate
+
+Used to verify the sever's ownership to sending encrypted data.
+
+## Certificate Authority
+
+A trusted organization that issues digital certificates.
+https://letsencrypt.org/
+
+## Self-Signed Certificate
+
+Enable HTTPS but not trusted by others (useful for development).
+
+## CA-Signed Certificate
+
+Trusted by most clients on the web (useful for production).
---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.get("/secret", (req, res) => {
+  return res.status(200).send("Your personal secret value is 69!");
+});
+
+app.get("/", (req, res) => {
+  return res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}...`);
+});
