f390fbd2dc79f1375dfaecc35e37880478cf918b
Hai Trieu Ngoc <trieungochai.dev@gmail.com>
2022-06-28 15:02:36 +0700

    Authentication Endpoints with Middleware

--- server.js
+++ server.js
@@ -10,7 +10,26 @@ app.use(helmet());
 
 const PORT = process.env.PORT || 3000;
 
-app.get("/secret", (req, res) => {
+function checkLoggedIn(req, res, next) {
+  const isLoggedIn = true; // TODO
+  if (!isLoggedIn) {
+    return res.status(401).json({
+      error: "You must log in!",
+    });
+  }
+
+  next();
+}
+
+app.get("/auth/google", (req, res) => {});
+
+app.get("/auth/google/callback", (req, res) => {});
+
+app.get("/auth/logout", (req, res) => {
+  
+});
+
+app.get("/secret", checkLoggedIn, (req, res) => {
   return res.status(200).send("Your personal secret value is 69!");
 });
 
