e732f63471f6439753813e3a653ff671a8f80606
Hai Trieu Ngoc <trieungochai.dev@gmail.com>
2022-07-01 15:02:57 +0700

    Setting up OAuth Cookie Session

--- server.js
+++ server.js
@@ -5,17 +5,30 @@ const helmet = require("helmet");
 const path = require("path");
 const passport = require("passport");
 const { Strategy } = require("passport-google-oauth20");
+const cookieSession = require("cookie-session");
 
 const app = express();
 require("dotenv").config();
 
 app.use(helmet());
 
+app.use(
+  cookieSession({
+    name: "session",
+    maxAge: 24 * 60 * 60 * 1000,
+    keys: [config.COOKIE_KEY_1, config.COOKIE_KEY_2], // 'secret key'
+  })
+);
+
+app.use(passport.initialize());
+
 const PORT = process.env.PORT || 3000;
 
 const config = {
   CLIENT_ID: process.env.CLIENT_ID,
   CLIENT_SECRET: process.env.CLIENT_SECRET,
+  COOKIE_KEY_1: process.env.COOKIE_KEY_1,
+  COOKIE_KEY_2: process.env.COOKIE_KEY_2,
 };
 
 const AUTH_OPTIONS = {
